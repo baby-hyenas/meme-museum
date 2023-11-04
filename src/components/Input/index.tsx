fe16e23334f567d044106ea9ca44e624b93d9268
yhrhee <yhrhee5484@gmail.com>
2023-11-05 02:20:26 +0900

    fix: change asset name, fix style

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,9 +7,5 @@ type InputProps = {
 export const Input = ({className}: InputProps): JSX.Element => {
   return (
     <input className={`search-input-default ${className}`} placeholder="검색어를 입력해주세요." type="search"></input>
-    // <div className={`search-input-default ${className}`}>
-    //   <div className="input-line" />
-    //   <div className="text-wrapper">asdf</div>
-    // </div>
   );
 };
