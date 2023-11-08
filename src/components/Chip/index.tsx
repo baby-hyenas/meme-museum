c616bb104396209f0c3eec78490c2c6462522034
Youjeong Park <pyj0695@gmail.com>
2023-11-08 21:10:01 +0900

    feat : tag 추가

--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 type ChipProps = {
-	text: string,
-  onClicked?: () => void
+	text: string
 }
 
-export const Chip: React.FC<ChipProps> = ({text, onClicked}: ChipProps) => {
+export const Chip: React.FC<ChipProps> = ({text}: ChipProps) => {
+  const navigate = useNavigate();
+
+  const navigateResult = ()=>{
+    navigate(`/result?name=${text}&type=tag`)
+  }
+
 	return (
-		<div className="chip-button" onClick={onClicked}>
+		<div className="chip-button" onClick={navigateResult}>
 			<div className="text-wrapper">{text}</div>
 		</div>
 	);
