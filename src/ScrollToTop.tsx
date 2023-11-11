ddb2ff16b2340355654f0e374fb593a7cf15ed36
Youjeong Park <pyj0695@gmail.com>
2023-11-11 14:15:02 +0900

    fix : scroll top error

--- src/ScrollToTop.tsx
+++ src/ScrollToTop.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = () => {
+const ScrollToTop = (): JSX.Element => {
   const location = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.search]);
 
+  return <></>
 };
 
 export default ScrollToTop;
